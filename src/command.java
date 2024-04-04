d68422dc5bbbfe79e8b6f32ff3495fdca4e2d97a
ahmniab <124299773+ahmniab@users.noreply.github.com>
2024-04-04 23:40:14 +0200

    create needed classes

---
+++ src/command.java
@@ -0,0 +1,10 @@
+public class command {
+    private String bash;
+    private String cmd;
+    public command(String line){
+
+    }
+    String get_cmd(){
+        return this.cmd;
+    }
+}
---
+++ src/compilor.java
@@ -0,0 +1,2 @@
+public class compilor {
+}
