2b6fb107de9c453cc6a20567f64d84c032089b45
ahmniab <124299773+ahmniab@users.noreply.github.com>
2024-04-05 16:57:20 +0200

    create primary interface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        System.out.println("shell");
+
+        while (true) {
+            System.out.print("Enter a command> ");
+            String comnd = get_f_arg();
+            System.out.println(comnd);
+        }
+
+
+
+
+    }
+    public static String get_f_arg(){
+        Scanner inp = new Scanner(System.in);
+        String inp_line = inp.nextLine();
+        return inp_line.split(" \n")[0];
     }
 }
