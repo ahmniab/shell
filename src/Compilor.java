8484cedc45061417afa8f791b936f79f0ca8f777
ahmniab <124299773+ahmniab@users.noreply.github.com>
2024-04-05 23:53:48 +0200

    implement invoke_command fn

--- src/Compilor.java
+++ src/Compilor.java
@@ -1,9 +1,36 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public abstract class Compilor {
     public static void handle_command(String cmd)
     {
-        System.out.println(cmd);
+        invoke_command(cmd);
     }
     private static void invoke_command(String bash){
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        // Windows
+        processBuilder.command("cmd.exe", "/c", bash);
+
+        try {
+
+            Process process = processBuilder.start();
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("\nExited with error code : " + exitCode);
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }
