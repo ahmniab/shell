6846d5ad4d0123b70aa5c34855753a57e98a0111
ahmniab <124299773+ahmniab@users.noreply.github.com>
2024-04-05 18:05:00 +0200

    update inti_command()

--- src/Analyzer.java
+++ src/Analyzer.java
@@ -10,7 +10,11 @@ public class Analyzer {
         return new String[3];
     }
     private void init_commands(){
-
+        String[] lines =  git_lines();
+        commands = new Command[lines.length];
+        for (int i = 0 ; i < lines.length ; i++) {
+            commands[i] = new Command(lines[i]);
+        }
     }
     public String dos2bash(String dos){
         return null ;
