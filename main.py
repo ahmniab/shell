064f55aae01a8c1542644944fde62a9ddc672906
ahmniab <124299773+ahmniab@users.noreply.github.com>
2024-04-06 14:36:18 +0200

    finish task

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ import os
 import subprocess 
         
 
-def invoke_commands(dos : str) :
+def invoke_commands(dos) :
+    if dos.strip() == "exit":
+        exit(0)
+    
     params = dos.split()
-    print(params)
     if params[0] in commands :
-        if params[0] == 'cd' :
-            os.chdir(params[1])
-        else :
-            subprocess.run(params)
+        if params[0] == "cd" :
+            if len(params) == 1 or params[1] == '~' :
+                os.chdir(os.path.expanduser('~'))
+            else :
+                os.chdir(params[1])
             
+        else :
+            params[0] = commands[params[0]]
+            subprocess.run(params)
+    else :
+        print("Invalid command ")        
             
 
 def load_commands() -> dict:
@@ -26,8 +34,12 @@ def load_commands() -> dict:
 commands = load_commands()
 
 def main():
-    invoke_commands('ls -l')
-    # print('ls')
+    while 1 :
+        try :
+            print(os.getcwd() + '>' , end=' ')
+            invoke_commands(input())
+        except KeyboardInterrupt :
+            print()
 
 if __name__ == "__main__":
     main()
